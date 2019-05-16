8802388619d7b4341b94f646f8d6c0655708a22d
Ryan Callaghan <RyanCallaghan2020@u.northwestern.edu>
2019-05-15 22:07:45 -0500

    tenoseven

--- song_page.js
+++ song_page.js
@@ -1,3 +1,28 @@
+function sortList(id, sel, filter) {
+  var a, b, c, i, ii, iii, hit;
+  a = w3.getElements(id);
+  for (i = 0; i < a.length; i++) {
+    b = a[i].querySelectorAll(sel);
+    for (ii = 0; ii < b.length; ii++) {
+      hit = 0;
+      if (b[ii].innerText.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+        hit = 1;
+      }
+      c = b[ii].getElementsByTagName("*");
+      for (iii = 0; iii < c.length; iii++) {
+        if (c[iii].innerText.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+          hit = 1;
+        }
+      }
+      if (hit == 1) {
+        b[ii].style.display = "";
+      } else {
+        b[ii].style.display = "none";
+      }
+    }
+  }
+};
+
 function buildSongList() {
   var songList = document.getElementById("songList");
 
@@ -30,20 +55,20 @@ function buildSongList() {
       songList.appendChild(table_element);
     }
   }
-}
+};
 
-function filterClick () {
-  $(document).ready(function() {
-      $("#btnFilter").click(function() {
-          $("#songList tbody tr").show();
-          if($("#filter").val.length > 0) {
-              $("#songList tbody tr").filter(function(index, elm) {
-                  return $(elm).html().toUpperCase().indexOf($("#filter").val().toUpperCase()) < 0;
-              }).hide();
-          }
-      });
-  });
-}
+//function filterClick () {
+//  $(song_page).ready(function() {
+//      $("#btnFilter").click(function() {
+//         $("#songList tbody tr").show();
+//          if($("#filter").val.length > 0) {
+//              $("#songList tbody tr").filter(function(index, elm) {
+//                  return $(elm).html().toUpperCase().indexOf($("#filter").val().toUpperCase()) < 0;
+//              }).hide();
+//          }
+//      });
+//  });
+//}
 
 
 
